a73530df7f4eb21a6cc6aba4188fbd65f5663b8b
MUTINTA <JustinMutinta@hotmail.com>
2021-08-30 22:33:41 -0500

    2nd Pass. Able to get the correct guesses to show up

--- main.py
+++ main.py
@@ -6,10 +6,22 @@ word_list = ["ardvark", "baboon", "camel"]
 
 chosen_word = random.choice(word_list)
 
+print(f"psst...the word is {chosen_word}")
+
+display = []
+
+for _ in range(len(chosen_word)):
+    display += "-"
+
+print(display)
+
 guess = input("Guess a letter: ").lower()
 
-for letter in chosen_word:
+for position in range(len(chosen_word)):
+    letter = chosen_word[position]
     if letter == guess:
-        print("Right")
-    else:
-        print("Wrong")
+        display[position] = letter
+
+
+print(display)
+
